6eed55d190d670763726b07ce9ef6b36e33c941c
Ishani-29 <login2ishani@gmail.com>
2022-07-27 23:18:49 +0530

    selection sort

--- sort.c
+++ sort.c
@@ -80,3 +80,19 @@ void insertion_sort(int arr[], int n)
     return;
 }
 
+void selection_Sort(int arr[], int n)
+{
+    int i, j, min;
+    for (i=0; i<n-1; i++){
+        min = i;
+        for (j=i+1; j<n; j++){
+          if (arr[j] < arr[min]){min=j;}
+        }
+
+        int temp=arr[min];
+        arr[min]=arr[i];
+        arr[i]=temp;
+    }
+    return;
+}
+
