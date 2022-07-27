8927d0fc60d9fa9923cf2d998bd3141814b1f024
Ishani-29 <login2ishani@gmail.com>
2022-07-27 23:20:58 +0530

    merge sort

--- sort.c
+++ sort.c
@@ -110,3 +110,43 @@ void bubble_sort(int arr[], int n)
     return;
 }
 
+void merge(int arr[], int l, int m, int r)
+{
+    int i,j,k;
+    int n1=m-l+1;
+    int n2=r-m;
+    int arrL[n1], arrR[n2];
+
+    for (i=0; i<n1; i++){arrL[i]=arr[l+i];}
+    for (j=0; j<n2; j++){arrR[j]=arr[m+1+j];}
+
+    i=0;j=0;k=l;
+
+    while (i<n1 && j<n2){
+        if (arrL[i]<=arrR[j]){
+            arr[k]=arrL[i];
+            i++;
+        }
+        else{
+            arr[k]=arrR[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i<n1){arr[k]=arrL[i];i++;k++;}
+    while (j<n2){arr[k] = arrR[j];j++;k++;}
+    return;
+}
+ 
+void merge_sort(int arr[], int l, int r)
+{
+    if (l<r){
+        int m = l + (r - l) / 2;
+        merge_sort(arr, l, m);
+        merge_sort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+    return;
+}
+
