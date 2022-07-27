cdf2f4af5959503538b5dcc956808e982b2561f2
Ishani-29 <login2ishani@gmail.com>
2022-07-27 23:21:58 +0530

    quick sort

--- sort.c
+++ sort.c
@@ -150,3 +150,27 @@ void merge_sort(int arr[], int l, int r)
     return;
 }
 
+void quick_sort(int arr[], int first, int last){
+    int i, j, pivot, temp;
+    if(first<last){
+        pivot=first;
+        i=first;
+        j=last;
+        while(i<j){
+            while(arr[i]<=arr[pivot]&&i<last){i++;}
+            while(arr[j]>arr[pivot]){j--;}
+            if(i<j){
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+    temp=arr[pivot];
+    arr[pivot]=arr[j];
+    arr[j]=temp;
+    quick_sort(arr,first,j-1);
+    quick_sort(arr,j+1,last);
+    }
+    return;
+}
+
